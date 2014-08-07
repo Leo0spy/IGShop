74dc2b2e23e68fed57f6e26c7f08852d56fbcc91
LuaxY <yann.guineau@voidmx.net>
2014-08-07 19:02:19 +0200

    base

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>IGShop v0.2</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="main-menu">
+    <a class="on" href=""><img class="icon" src="imgs/main_shop.png" /> Boutique</a>
+    <a href=""><img class="icon" src="imgs/main_jetons.png" /> Jetons</a>
+    <a href=""><img class="icon" src="imgs/main_gift.png" /> Cadeaux</a>
+    <a href=""><img class="icon" src="imgs/main_vip.png" /> Premium</a>
+</div>
+<div id="container">
+    <div id="left">
+        <div id="menu">
+            <a class="on" href="">Acceuil</a>
+            <a href="">Promotions</a>
+            <a href="">Objets</a>
+            <a href="">Autres</a>
+        </div>
+        <div class="content">
+            <br />
+            <br />
+            <h2>Bienvenue sur la boutique !</h2>
+            <div class="message">Coucou <3</div>
+        </div>
+    </div>
+    <div id="right">
+        <div class="name"><img src="imgs/picto_avatar.png" />Luax</div>
+        <div class="server">Serveur: <b>Test</b></div>
+        <div class="money kamas">
+            <span class="title">Mes Kamas</span>
+            <div class="value">18 133 742 <img src="imgs/picto_kamas.png" /></div>
+        </div>
+        <div class="money jetons">
+            <span class="title">Mes Jetons</span>
+            <div class="value">666 <img src="imgs/picto_jetons.png" /></div>
+        </div>
+        <div class="buy"><a href="">Acheter des Jetons</a></div>
+        <div class="hr"></div>
+        <div class="links">
+            <a href="">Comment acheter des jetons</a>
+            <a href="">Fonctionnement de la boutique</a>
+            <a href="">Foire aux questions</a>
+            <a href="">Signaler un problème</a>
+        </div>
+    </div>
+    <div id="powered">Powered by IGShop v0.2</div>
+</div>
+</body>
+</html>
